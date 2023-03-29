8569f61bfccdbba62322f1352b6f9eae3bbf882d
warzes <warzes@mail.ru>
2023-03-29 14:01:10 +1000

    - Project

---
+++ src/Game/main.cpp
@@ -0,0 +1,18 @@
+﻿#include "stdafx.h"
+//-----------------------------------------------------------------------------
+#if defined(_MSC_VER)
+#	pragma comment( lib, "3rdparty.lib" )
+#	pragma comment( lib, "Engine.lib" )
+#endif
+//-----------------------------------------------------------------------------
+int main(
+	[[maybe_unused]] int   argc,
+	[[maybe_unused]] char* argv[])
+{
+	extern void fnMy3rdparty(); // 3rdparty.lib
+	extern void fnEngine();     // Engine.lib
+
+	fnMy3rdparty();
+	fnEngine();
+}
+//-----------------------------------------------------------------------------
